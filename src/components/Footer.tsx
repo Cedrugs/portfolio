d5b8bf337ebb95efbb7332e1c6c7947875ccaa33
Cedrugs <ceds.sam@gmail.com>
2025-04-01 11:15:12 +0700

    Change color palette

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,44 +6,44 @@ export default function Footer() {
                     <div>
                         <ul className="flex flex-wrap list-none gap-12 text-2xl">
                             <li className="group relative inline-block">
-                                <a href="#About" className="relative hover:text-gray-600 duration-500 transition-all ease-in-out">
+                                <a href="#About" className="relative hover:text-gray-600 duration-500 transition-all ease-in-out text-[#353535]">
                                     About
                                     <span className="absolute top-10 left-0 w-full h-[2px] bg-current transform scale-x-0 transition-transform duration-500 ease-in-out group-hover:scale-x-100"></span>
                                 </a>
                             </li>
                             <li className="group relative inline-block">
-                                <a href="#Experience" className="relative hover:text-gray-600 duration-500 transition-all ease-in-out">
+                                <a href="#Experience" className="relative hover:text-gray-600 duration-500 transition-all ease-in-out text-[#353535]">
                                     Experience
                                     <span className="absolute top-10 left-0 w-full h-[2px] bg-current transform scale-x-0 transition-transform duration-500 ease-in-out group-hover:scale-x-100"></span>
                                 </a>
                             </li>
                             <li className="group relative inline-block">
-                                <a href="#TechStack" className="relative hover:text-gray-600 duration-500 transition-all ease-in-out">
+                                <a href="#TechStack" className="relative hover:text-gray-600 duration-500 transition-all ease-in-out text-[#353535]">
                                     Tech Stack
                                     <span className="absolute top-10 left-0 w-full h-[2px] bg-current transform scale-x-0 transition-transform duration-500 ease-in-out group-hover:scale-x-100"></span>
                                 </a>
                             </li>
                             <li className="group relative inline-block">
-                                <a href="#Projects" className="relative hover:text-gray-600 duration-500 transition-all ease-in-out">
+                                <a href="#Projects" className="relative hover:text-gray-600 duration-500 transition-all ease-in-out text-[#353535]">
                                     Projects
                                     <span className="absolute top-10 left-0 w-full h-[2px] bg-current transform scale-x-0 transition-transform duration-500 ease-in-out group-hover:scale-x-100"></span>
                                 </a>
                             </li>
                             <li className="group relative inline-block">
-                                <a href="#Contact" className="relative hover:text-gray-600 duration-500 transition-all ease-in-out">
+                                <a href="#Contact" className="relative hover:text-gray-600 duration-500 transition-all ease-in-out text-[#353535]">
                                     Contact
                                     <span className="absolute top-10 left-0 w-full h-[2px] bg-current transform scale-x-0 transition-transform duration-500 ease-in-out group-hover:scale-x-100"></span>
                                 </a>
                             </li>
                             <li className="group relative inline-block">
-                                <a href="https://links.samuelcedric.com" className="relative hover:text-gray-600 duration-500 transition-all ease-in-out">
+                                <a href="https://links.samuelcedric.com" className="relative hover:text-gray-600 duration-500 transition-all ease-in-out text-[#353535]">
                                     Links
                                     <span className="absolute top-10 left-0 w-full h-[2px] bg-current transform scale-x-0 transition-transform duration-500 ease-in-out group-hover:scale-x-100"></span>
                                 </a>
                             </li>
                         </ul>
                     </div>
-                    <p className="py-10 text-xl">© Samuel Cedric. All Rights Reserved.</p>
+                    <p className="py-10 text-xl text-[#353535]">© Samuel Cedric. All Rights Reserved.</p>
                 </nav>
             </footer>
         </>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -78,7 +78,7 @@ export default function Home() {
                 </div>
                 <div className="flex flex-col justify-center items-center gap-4 max-lg:gap-2">
                     <p className="font-semibold text-gray-600 max-lg:text-sm">Hello I'm</p>
-                    <h1 className="font-semibold text-5xl max-lg:text-4xl">Samuel Cedric</h1>
+                    <h1 className="font-semibold text-5xl max-lg:text-4xl text-[#353535]">Samuel Cedric</h1>
                     <h2 className="font-semibold text-3xl text-gray-600 max-lg:text-2xl">Full Stack Developer</h2>
                     <div className="flex gap-4 mt-2">
                         <Button href="#" text="Download CV" type={1} />
@@ -101,24 +101,24 @@ export default function Home() {
                 <div className="flex flex-wrap flex-col container gap-20">
                     <div className="flex flex-col justify-center items-center gap-4 pb-10">
                         <h2 className="font-medium text-gray-600">Get to know more</h2>
-                        <h1 className="font-semibold text-5xl">About Me</h1>
+                        <h1 className="font-semibold text-5xl text-[#353535]">About Me</h1>
                     </div>
                     <div className="flex flex-wrap lg:gap-10 justify-center items-center container">
-                        <div className="flex flex-col border-gray-600 border-2 rounded-3xl w-[45rem] h-[15rem] justify-center items-center text-center gap-1">
+                        <div className="flex flex-col border-[#353535] border-2 rounded-3xl w-[45rem] h-[15rem] justify-center items-center text-center gap-1">
                             <i className="material-icons text-3xl">workspace_premium</i>
-                            <h3 className="text-lg font-semibold">Experience</h3>
+                            <h3 className="text-lg font-semibold text-[#353535]">Experience</h3>
                             <p className="text-gray-800">DevOps Engineering</p>
                             <p className="text-gray-800">Back End & Front End Development</p>
                         </div>
                         <div className="flex flex-col border-gray-600 border-2 rounded-3xl w-[45rem] h-[15rem] justify-center items-center text-center gap-1">
                             <i className="material-icons text-3xl">school</i>
-                            <h3 className="text-lg font-semibold">Education</h3>
+                            <h3 className="text-lg font-semibold text-[#353535]">Education</h3>
                             <p className="text-gray-800">Highschool Degree in Science</p>
                             <p className="text-gray-800">Bachelors Degree in Computer Science by 2028</p>
                         </div>
                     </div>
                     <div>
-                        <p className="text-justify text-lg px-10">Hey, I'm Samuel Cedric, a Fullstack Developer currently studying Computer Science at BINUS University. At 18, I'm really passionate about coding and technology, and I'm always looking for ways to make things more efficient and innovative. Ever since I was young, I've been fascinated by how software development can change the way we interact with technology. My goal is to help people and organizations harness the amazing potential of coding. As I get ready to start this exciting new chapter in my academic and professional journey, I invite you to join me in exploring how technology can lead to positive change in our world.</p>
+                        <p className="text-justify text-lg px-10 text-[#353535]">Hey, I'm Samuel Cedric, a Fullstack Developer currently studying Computer Science at BINUS University. At 18, I'm really passionate about coding and technology, and I'm always looking for ways to make things more efficient and innovative. Ever since I was young, I've been fascinated by how software development can change the way we interact with technology. My goal is to help people and organizations harness the amazing potential of coding. As I get ready to start this exciting new chapter in my academic and professional journey, I invite you to join me in exploring how technology can lead to positive change in our world.</p>
                     </div>
                 </div>
             </section>
@@ -126,17 +126,17 @@ export default function Home() {
                 <div className="container flex flex-wrap flex-col gap-20">
                     <div className="flex flex-col justify-center items-center gap-4 pb-10">
                         <h2 className="font-medium text-gray-600">Have a look at my</h2>
-                        <h1 className="font-semibold text-5xl">Experience</h1>
+                        <h1 className="font-semibold text-5xl text-[#353535]">Experience</h1>
                     </div>
                     <div className="flex flex-wrap container gap-10 justify-center items-center">
                         <div className="flex flex-col border-gray-600 border-2 rounded-3xl w-[45rem] py-14 justify-center items-center text-center gap-4">
-                            <h3 className="text-3xl font-semibold">Frontend Development</h3>
+                            <h3 className="text-3xl font-semibold text-[#353535]">Frontend Development</h3>
                             <div className="grid grid-cols-3 gap-10 px-16 py-8">
                                 {experiences.frontend.map((experience) => (
                                     <article className="container grid grid-cols-3 text-left w-32">
-                                        <i className="material-icons">verified</i>
+                                        <i className="material-icons text-[#353535]">verified</i>
                                         <div>
-                                            <h3 className="font-semibold text-lg">{experience.name}</h3>
+                                            <h3 className="font-semibold text-lg text-[#353535]">{experience.name}</h3>
                                             <p className="text-gray-600">{experience.level}</p>
                                         </div>
                                     </article>
@@ -144,13 +144,13 @@ export default function Home() {
                             </div>
                         </div>
                         <div className="flex flex-col border-gray-600 border-2 rounded-3xl w-[45rem] py-14 justify-center items-center text-center gap-4">
-                            <h3 className="text-3xl font-semibold">Backend Development</h3>
+                            <h3 className="text-3xl font-semibold text-[#353535]">Backend Development</h3>
                             <div className="grid grid-cols-3 gap-10 px-16 py-8">
                                 {experiences.backend.map((experience) => (
                                     <article className="grid grid-cols-3 text-left w-32">
-                                        <i className="material-icons">verified</i>
+                                        <i className="material-icons text-[#353535]">verified</i>
                                         <div>
-                                            <h3 className="font-semibold text-lg">{experience.name}</h3>
+                                            <h3 className="font-semibold text-lg text-[#353535]">{experience.name}</h3>
                                             <p className="text-gray-600">{experience.level}</p>
                                         </div>
                                     </article>
@@ -164,12 +164,12 @@ export default function Home() {
                 <div className="container flex flex-wrap flex-col justify-center items-center">
                     <div className="flex flex-col justify-center items-center gap-4 pb-10">
                         <h2 className="font-medium text-gray-600">Check out my current</h2>
-                        <h1 className="font-semibold text-5xl">Tech Stack</h1>
+                        <h1 className="font-semibold text-5xl text-[#353535]">Tech Stack</h1>
                     </div>
                     <div className="flex flex-wrap gap-5 justify-center items-center">
                         {techStacks.map((stack, index) => (
-                            <div key={index} className="flex justify-center items-center">
-                                <i className={`${stack} border rounded-md border-black p-8 text-5xl`}></i>
+                            <div key={index} className="flex justify-center items-center hover:shadow-xl hover:scale-110 duration-500 ease-in-out">
+                                <i className={`${stack} border-2 rounded-xl border-[#353535] text-[#353535] p-8 text-5xl`}></i>
                             </div>
                         ))}
                     </div>
@@ -179,11 +179,11 @@ export default function Home() {
                 <div className="container flex flex-wrap flex-col justify-center items-center">
                     <div className="flex flex-col justify-center items-center gap-4 pb-10">
                         <h2 className="font-medium text-gray-600">Browse my recent</h2>
-                        <h1 className="font-semibold text-5xl">Project</h1>
+                        <h1 className="font-semibold text-5xl text-[#353535]">Projects</h1>
                     </div>
                     <div className="flex flex-wrap justify-around items-center gap-10">
                         {projects.map((project, index) => (
-                            <div key={index} className="flex flex-col justify-center items-center border-gray-600 border-2 p-10 rounded-3xl gap-6">
+                            <div key={index} className="flex flex-col justify-center items-center border-[#353535] border-2 p-10 rounded-3xl gap-6 hover:shadow-xl hover:scale-[1.05] duration-500 ease-in-out">
                                 <div>
                                     <img src={project.icon} alt="Project 1 Image" className="rounded-2xl w-[350px] h-[350px]"/>
                                 </div>
@@ -202,8 +202,8 @@ export default function Home() {
                         <h2 className="font-medium text-gray-600">Get in touch!</h2>
                         <h1 className="font-semibold text-5xl">Contact Me</h1>
                     </div>
-                    <div className="flex justify-center items-center border-gray-600 border-2 p-8 rounded-[2.5rem] gap-6">
-                        <a href="mailto:ceds.sam@gmail.com" className="flex items-center justify-center w-8 h-8 border-2 border-black rounded-full bg-black hover:opacity-70 transition-all ease-in-out duration-500">
+                    <div className="flex justify-center items-center border-[#353535] border-2 p-8 rounded-[2.5rem] gap-6">
+                        <a href="mailto:ceds.sam@gmail.com" className="flex items-center justify-center w-8 h-8 border-2 border-[black] rounded-full bg-black hover:opacity-70 transition-all ease-in-out duration-500">
                             <i className="fa-solid fa-envelope text-white"></i>
                         </a>
                         <a href="mailto:ceds.sam@gmail.com" className="group relative hover:text-gray-600 duration-500 transition-all ease-in-out text-xl">
